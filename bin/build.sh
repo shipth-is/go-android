38d28758deb9201463b42f36219641c52965acb6
David Sutherland <sutherland.dave@gmail.com>
2025-12-05 16:46:13 +0000

    Use .env in the build script

--- bin/build.sh
+++ bin/build.sh
@@ -4,6 +4,13 @@ set -euo pipefail
 scriptDir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$scriptDir/.."
 
+if [ ! -f .env ]; then
+  echo ".env file not found!"
+  exit 1
+fi
+
+export $(grep -v '^#' .env | xargs)
+
 # Flags
 skipClean=false
 copyLocalLibs=false
